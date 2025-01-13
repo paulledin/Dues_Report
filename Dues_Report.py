a220ee73c7223b65e41821b260e8d8cf75215f76
Paul Ledin <paul_ledin@uwalumni.com>
2025-01-13 17:36:45 -0600

    Update Dues_Report.py

--- Dues_Report.py
+++ Dues_Report.py
@@ -66,6 +66,7 @@ else:
                 st.markdown('#### !! No Credit Unions Found Matching NIMBLE_CUNA_ID -> ' + nimble_cuna_id + ' !!')
             else:
                 st.markdown('NIMBLE_CUNA_ID: ' + nimble_cuna_id)
+                st.markdown(thisCU['NAME'])
         
 
     with col[1]:

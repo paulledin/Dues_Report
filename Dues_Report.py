6af09599f212f7b63facacad257efa336cba204e
Paul Ledin <paul_ledin@uwalumni.com>
2025-01-21 16:29:48 -0600

    Update Dues_Report.py

--- Dues_Report.py
+++ Dues_Report.py
@@ -39,7 +39,7 @@ def getLeagueNames():
 def getCUDuesPremlimEst(nimble_cuna_id):
     return (dbConn.session().sql("SELECT status, afl, league_affiliated, nafcu_affiliated, current_members, current_assets, june_assets, num_mergers, cuna_dues_2025, nafcu_dues_2025, full_amt_2025, expected_dues, formula FROM acus_data.dues.dues_est_2025 WHERE nimble_cuna_id='" + nimble_cuna_id + "' ").to_pandas())
 
-def expandStatus(df):
+def expandFlagDescriptions(df):
     df.loc[df['STATUS'] == 'A', 'STATUS'] = 'Active'
     df.loc[df['STATUS'] == 'P', 'STATUS'] = 'Pending'
     df.loc[df['STATUS'] == 'L', 'STATUS'] = 'Liquidated'
@@ -78,7 +78,7 @@ else:
         st.markdown('#### 2025 Dues Calculation')
         if (selected_report_type == 'Individual CU'):
             thisCU = getCUData(nimble_cuna_id)
-            prelimDues = expandStatus(getCUDuesPremlimEst(nimble_cuna_id))
+            prelimDues = expandFlagDescriptions(getCUDuesPremlimEst(nimble_cuna_id))
             
             if(len(thisCU) == 0):
                 st.markdown('#### !! No Credit Unions Found Matching NIMBLE_CUNA_ID -> ' + nimble_cuna_id + ' !!')

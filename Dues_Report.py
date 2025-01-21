2f13a9c8b5edd3cb936b2c8b6a8d43fbfbb5db6d
Paul Ledin <paul_ledin@uwalumni.com>
2025-01-21 11:41:43 -0600

    Update Dues_Report.py

--- Dues_Report.py
+++ Dues_Report.py
@@ -70,7 +70,7 @@ else:
                 st.markdown(thisCU['NAME'].loc[thisCU.index[0]])
                 st.markdown(thisCU['ST_ADDRESS'].loc[thisCU.index[0]] + ', ' + thisCU['ST_STATE'].loc[thisCU.index[0]] + ' ' + thisCU['ST_ZIP_CODE'].loc[thisCU.index[0]])
                 
-                txt = "Current Members:  {members:.n0} "
+                txt = "Current Members:  {members:.0n} "
                 st.markdown(txt.format(members = thisCU['MEMBERS'].loc[thisCU.index[0]]))
 
     

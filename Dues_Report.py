93c11b7bef5a60825e3c45879f46cb882c58af38
Paul Ledin <paul_ledin@uwalumni.com>
2025-01-21 10:50:08 -0600

    Update Dues_Report.py

--- Dues_Report.py
+++ Dues_Report.py
@@ -69,7 +69,7 @@ else:
                 st.markdown(thisCU['NIMBLE_CUNA_ID'].loc[thisCU.index[0]])
                 st.markdown(thisCU['NAME'].loc[thisCU.index[0]])
                 st.markdown(thisCU['ST_ADDRESS'].loc[thisCU.index[0]] + ', ' + thisCU['ST_STATE'].loc[thisCU.index[0]] + ' ' + thisCU['ST_ZIP_CODE'].loc[thisCU.index[0]])
-                st.markdown('Current Members : ' + thisCU['MEMBERS'].loc[thisCU.index[0]])
+                st.markdown(thisCU['MEMBERS'].loc[thisCU.index[0]])
         
 
     with col[1]:

f1e34c6f58deecfdfc1d923799685f08c600c772
Paul Ledin <paul_ledin@uwalumni.com>
2025-01-22 15:24:03 -0600

    Update Dues_Report.py

--- Dues_Report.py
+++ Dues_Report.py
@@ -119,8 +119,8 @@ else:
 
                 current_members = "**March 2024 Members:**  {members:,.0f} "
                 st.markdown(current_members.format(members = prelimDues['CURRENT_MEMBERS'].loc[prelimDues.index[0]]))
-                current_assets = "**March 2024 Assets:**  ${assets:,.0f} "
-                st.markdown(current_assets.format(assets = prelimDues['CURRENT_ASSETS'].loc[prelimDues.index[0]]))
+                current_assets = "**June 2024 Assets:**  ${assets:,.0f} "
+                st.markdown(current_assets.format(assets = prelimDues['JUNE_ASSETS'].loc[prelimDues.index[0]]))
                 st.write(mergers)
                 
                 st.markdown('---')

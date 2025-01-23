500d4d2b4e348c1e2f1dd048edcd5e7475876c94
Paul Ledin <paul_ledin@uwalumni.com>
2025-01-22 18:38:40 -0600

    Update Dues_Report.py

--- Dues_Report.py
+++ Dues_Report.py
@@ -166,7 +166,7 @@ else:
 
                 st.markdown("**Legacy CUNA Cap Calculation:**")
 
-                dues_calc = (prelimDues['CURRENT_MEMBERS'].loc[prelimDues.index[0]] * 0.12) + (prelimDues['CURRENT_ASSETS'].loc[prelimDues.index[0]] *  0.000018)
+                dues_calc = round((prelimDues['CURRENT_MEMBERS'].loc[prelimDues.index[0]] * 0.12) + (prelimDues['CURRENT_ASSETS'].loc[prelimDues.index[0]] *  0.000018), 0)
 
                 cuna_cap_calc = "({members:,.0f} * 0.12) + (${assets:,.0f} * 0.000018) = " + str(dues_calc)
                 st.markdown(cuna_cap_calc.format(members = prelimDues['CURRENT_MEMBERS'].loc[prelimDues.index[0]], assets = prelimDues['CURRENT_ASSETS'].loc[prelimDues.index[0]]))

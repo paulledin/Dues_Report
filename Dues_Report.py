e6d930180f47892748ba4a9a0a0991271f946122
Paul Ledin <paul_ledin@uwalumni.com>
2025-01-27 11:32:40 -0600

    Update Dues_Report.py

--- Dues_Report.py
+++ Dues_Report.py
@@ -176,7 +176,7 @@ else:
                 dues_calc = prelimDues['CURRENT_ASSETS'].loc[prelimDues.index[0]] * 0.000018 + prelimDues['CURRENT_MEMBERS'].loc[prelimDues.index[0]] * 0.12
 
                 #prelimDues['CURRENT_MEMBERS'].loc[prelimDues.index[0]] * 0.12)
-                st.markdown(txt.format(dues = dues_calc)
+                st.markdown(txt.format(dues = dues_calc))
                 #st.markdown(txt.format(price = 49)) 
 
 
